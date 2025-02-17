7dfba7ce460ce9eab42d57a86dd2d4a8e2c9d76c
Akash Das <addy.ce@rediffmail.com>
2025-02-17 22:39:09 +0100

    Gradio

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import gradio as gr
-from movie_recommendation_system import get_similar_movies # The movie-recommendation-system.ipynb file should be converted to .py file for this import to work
+from movie_recommendation_system import get_similar_movies # Import from movie-recommendation-system.py file
 
 interface = gr.Interface(fn=get_similar_movies,
                         inputs=gr.Textbox(lines=2, placeholder="Enter a movie title"),
